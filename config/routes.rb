1e147da92940249254852df44f0b6132e68ee3a6
Nico Proto <nicolasproto@gmail.com>
2021-04-26 23:23:50 +0200

    FEAT: Added CRUD actions for tweets and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
   root to: 'pages#home'
+
+  resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
