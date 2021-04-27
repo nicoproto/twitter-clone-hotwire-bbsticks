c63399c4dbd9e46d34512c38b53e13d4ea9b4fef
Nico Proto <nicolasproto@gmail.com>
2021-04-27 08:26:47 +0200

    FEAT: Added tweets basic views for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
-  root to: 'pages#home'
+  root to: "tweets#index"
 
-  resources :tweets
+  resources :tweets do
+    resource :like
+    resource :retweet
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
